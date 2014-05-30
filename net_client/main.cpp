d024dca3fa53db38ff586f58e6951c8a9e668628
dycforever <dycforever@126.com>
2014-05-30 13:33:06 +0800

    test Condition

--- net_client/main.cpp
+++ net_client/main.cpp
@@ -3,28 +3,31 @@
 #include "Mutex.h"
 #include "InetAddress.h"
 #include "EventLoop.h"
-
+#include "Condition.h"
 #include <boost/bind.hpp>
 
-
+using namespace std;
 typedef dyc::Socket* SocketPtr;
 class HttpResponseParse {
 
 public:
-    HttpResponseParse():mPos(0){}
+    HttpResponseParse():mPos(0), mCond(mLock){}
 
     int readData(SocketPtr socket) {
         char buf[1024];
         int count = socket->recv(buf, sizeof(buf));
-        std::string str;
-        str.append(buf, count);
-        mLock.unlock();
+
+        cout << "recv " << count << " bytes" << endl;
+
+        mResponse.append(buf, count);
+        mCond.notify();
     }
 
     int conn(SocketPtr socket) {
         std::string reqLine ="GET /gz2 HTTP/1.1\r\nHost: localhost\r\n\r\n";
         int count = socket->send(reqLine.c_str(), reqLine.size()-mPos);
         mPos += count;
+        cout << "send " << count << " bytes" << endl;
     }
 
     std::string out() {
@@ -32,13 +35,17 @@ public:
     }
 
     void wait() {
-        mLock.lock();
+        cout << "will wait" << endl;
+        mCond.wait();
+        mCond.wait();
+        cout << "awake" << endl;
     }
 
 private:
     std::string mResponse;
     int mPos;
     dyc::MutexLock mLock;
+    dyc::Condition mCond;
 };
 
 
@@ -46,6 +53,20 @@ private:
 int main() {
     using namespace dyc;
     using namespace std;
+
+    MutexLock lock;
+    Condition cond(lock);
+    cond.wait();
+    cout << "hehe" << endl;
+
+    pthread_cond_t pc;
+    cout << pthread_cond_init(&pc, NULL) << endl;
+    lock.lock();
+    cout << pthread_cond_wait(&pc, lock.getPthreadMutex()) << endl;
+    lock.unlock();
+    cout << "hehe" << endl;
+
+
     Socket sock(true);
 
     InetAddress addr("127.0.0.1", 8714);
